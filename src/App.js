6d45b201ccc0192d2dc81f59e0f8d26894b3f55f
Justin Hwang <jstnhwang@gmail.com>
2021-05-19 20:28:25 +0100

    Import Movies compoenent in App.js for render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Movies from "./components/movies";
 
 function App() {
   return (
     <main className="container">
-      <p>Hi</p>
+      <Movies />
     </main>
   );
 }
