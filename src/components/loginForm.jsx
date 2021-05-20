af10981158a19852d33ca5170ca1af1c9ed27cb1
Justin Hwang <jstnhwang@gmail.com>
2021-05-21 00:02:43 +0100

    Add login, password input and submit to Login form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,7 +2,28 @@ import React, { Component } from "react";
 
 class LoginForm extends Component {
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username
+            </label>
+            <input type="username" className="form-control" id="username" aria-describedby="username" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input type="password" className="form-control" id="password" />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
