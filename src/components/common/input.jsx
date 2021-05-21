c1acffe5bb8233831b4f9d7abdd42a7b1c83a18b
Justin Hwang <jstnhwang@gmail.com>
2021-05-22 00:16:30 +0100

    Finish login form and extract all resuable parts

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 
-const Input = ({ name, label, value, type, error, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="mb-3">
       <label htmlFor={name} className="form-label">
         {label}
       </label>
-      <input
-        value={value}
-        name={name}
-        type={type}
-        className="form-control"
-        id={name}
-        onChange={onChange}
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,74 +1,30 @@
-import React, { Component } from "react";
-import Input from "./common/input";
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {}
   };
 
-  validate = () => {
-    const errors = {};
-    if (this.state.account.username.trim() === "") errors.username = "Username is required.";
-    if (this.state.account.password.trim() === "") errors.password = "Password is required.";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required()
   };
 
-  validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required.";
-    }
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required.";
-    }
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-
-    this.setState({ account, errors });
+  doSubmit = () => {
+    console.log("Submitted");
   };
 
   render() {
-    const { account, errors } = this.state;
-
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            label="Username"
-            value={account.username}
-            type="text"
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            label="Password"
-            value={account.password}
-            type="password"
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <button type="submit" className="btn btn-primary">
-            Login
-          </button>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
