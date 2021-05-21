37ce88a36476d8fdb3f893774d3c70c0c518d8e6
Justin Hwang <jstnhwang@gmail.com>
2021-05-21 22:56:06 +0100

    Implement loginForm for basic functionality

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,22 +1,52 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
+
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="mb-3">
             <label htmlFor="username" className="form-label">
               Username
             </label>
-            <input type="username" className="form-control" id="username" aria-describedby="username" />
+            <input
+              value={account.username}
+              name="username"
+              type="text"
+              className="form-control"
+              id="username"
+              onChange={this.handleChange}
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="password" className="form-label">
               Password
             </label>
-            <input type="password" className="form-control" id="password" />
+            <input
+              value={account.password}
+              name="password"
+              type="password"
+              className="form-control"
+              id="password"
+              onChange={this.handleChange}
+            />
           </div>
           <button type="submit" className="btn btn-primary">
             Login
