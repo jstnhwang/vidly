6c0ff18caac3242a08fa1f33d583667a51ae39ac
Justin Hwang <jstnhwang@gmail.com>
2021-05-26 22:07:50 +0100

    Build reusable Select component

---
+++ src/components/common/select.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Select = ({ name, label, options, error, ...rest }) => {
+  return (
+    <div className="mb-3">
+      <label htmlFor={name} className="form-label">
+        {label}
+      </label>
+      <select name={name} id={name} {...rest} className="form-select">
+        <option selected></option>
+        {options.map(option => (
+          <option key={option._id} value={option._id}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+export default Select;
